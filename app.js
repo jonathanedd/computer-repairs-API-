d189286f16595127bb83ce1159b5977c4bae2f55
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2022-05-24 01:44:12 -0500

    Connecting to Heroku

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
 
 // controller
 const { globalErrorHandler } = require("./controllers/error.controller");
@@ -10,8 +16,25 @@ const { repairRouter } = require("./routes/repairs.routes");
 //Initialize express app
 const app = express();
 
+app.use(cors());
+
 //enable incoming JSON data
 app.use(express.json());
+
+// Limit IP request
+const limiter = rateLimit({
+  max: 10000,
+  windowMs: 1 * 60 * 60 * 1000,
+  message: "Too many requests from this IP",
+});
+
+app.use(limiter);
+app.use(helmet());
+app.use(compression());
+
+// Log incoming requests
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+else app.use(morgan("combined"));
 
 // URL : http://localhost:4001/api/v2/users
 app.use("/api/v2/users", userRouter);
